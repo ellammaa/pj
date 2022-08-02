e626404795d39750d8a43738f430801a000b6bc8
ellammaa <110462383+ellammaa@users.noreply.github.com>
2022-08-02 15:50:18 +0530

    Add files via upload

---
+++ sub_practice.py
@@ -0,0 +1,70 @@
+# from flask import Flask,request
+# app=Flask(__name__)
+# # # @ app.route('/')
+# # # def fun1():
+# #     return "sample app"
+#
+# @app.route('/add')
+# def addition():
+#     a=request.args.get('a')
+#     b=request.args.get('b')
+#     return str(int(a)+int(b))
+# app.run()
+
+# @app.route('/add')
+# def add1():
+#     a=request.args.get('a')
+#     b=request.args.get('b')
+#     return str(int(a)+int(b))
+# app.run()
+
+
+# @app.route ('/sub')
+# def subtraction():
+#     c=request.args.get('c')
+#     d=request.args.get('d')
+#     return str (int(c)-int(d))
+# app.run()
+
+# @app.route('/sub')
+# def sub1():
+#     c=request.args.get('c')
+#     d=request.args.get('d')
+#     return str (int(c)-int(d))
+# app.run()
+
+
+# a=23
+# b=2
+# @app.route ('/mul')
+# def multiplication():
+#     return str(a*b)
+
+# @app.route('/div')
+# def division():
+#     a=request.args.get('a')
+#     b=request.args.get('b')
+#     return str (int(a)/int(b))
+# app.run()
+
+# @app.route('/div')
+# def div1():
+#     d=request.args.get('d')
+#     h=request.args.get('h')
+#     return str (int(d)/int(h))
+# app.run()
+#
+# @app.route('/mod')
+# def module():
+#     c=request.args.get('c')
+#     d=request.args.get('d')
+#     return str (int(c)%int(d))
+# app.run()
+
+# @app.route ('/add')
+# def module():
+#     a=request.args.get('a')
+#     d=request.args.get('d')
+#     return str (int(a)+int(d))
+# app.run()
+#
